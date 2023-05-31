c2a305862f5325c6a00439555da996fe1bf8af63
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2023-05-31 21:25:17 +0300

    створює та додає Li з массиву

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.getElementById("ingredients")
+
+const listItemsArr = [];
+
+ingredients.forEach(ingredient => {
+const listItem = document.createElement('li');
+listItem.textContent = ingredient;
+listItem.classList.add('items');
+console.log(listItem);
+listItemsArr.push(listItem);
+});
+
+listEl.append(...listItemsArr);
+
