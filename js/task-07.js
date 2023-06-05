ed539343474ae6a5ca20450e3770e0c6706ca54f
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2023-06-05 21:54:23 +0300

    Змінює значення font-size task-7

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const sizeControl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+
+sizeControl.addEventListener('input', event => {
+textEl.style.fontSize = `${event.target.value}px`
+});
