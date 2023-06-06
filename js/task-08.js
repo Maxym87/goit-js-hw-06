aae61af12bfed1e1b9a7ea9e0b0a4d98c9964f31
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2023-06-06 21:22:57 +0300

    виконує task-8

--- js/task-08.js
+++ js/task-08.js
@@ -5,20 +5,19 @@ formEl.addEventListener("submit", hundleSubmit);
 function hundleSubmit(event) {
   event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-const {
-  elements: { email, password },
-} = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Усі поля мають бути заповнені");
+  }
 
-if (email.value === "" || password.value === "") {
-  return alert("Усі поля мають бути заповнені");
-}
+  const userInfo = {
+    email: email.value,
+    password: password.value,
+  };
 
-const userInfo = {
-  email: email.value,
-  password: password.value,
-};
-
-console.log(userInfo);
-event.currentTarget.reset();
+  console.log(userInfo);
+  event.currentTarget.reset();
 }
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color");
+
+changeColorBtn.addEventListener("click", () => {
+  let randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+
+  spanColor.textContent = randomColor;
+});
